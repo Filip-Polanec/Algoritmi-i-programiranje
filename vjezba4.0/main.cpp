0d3e01a445fde13df1b61ece4ef4387f00b62ca5
Filip-Polanec <polanecfilip@gmail.com>
2025-05-22 18:16:11 +0200

    main.cpp

--- vjezba4.0/main.cpp
+++ vjezba4.0/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+    //lijepo ispisano 2D polje [10][10] svaki red se povećava za jedan
+    int p[10][10];
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            p[i][j]=i;
+        cout<<p[i][j]<<"\t";
+    }
+    cout<<endl;
+    }
     int p[10][10];
     for (int i=0;i<10;i++){
         for (int j=0;j<10;j++){
@@ -14,7 +23,16 @@ int main()
     }
     cout<<endl;
     }
-
+    cout<<endl;
+    //lijepo ispisano 2D polje [10][10] svaki stupac se povećava za jedan
+    int p[10][10];
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            p[i][j]=j;
+        cout<<p[i][j]<<"\t";
+    }
+    cout<<endl;
+    }
     return 0;
 
 }
